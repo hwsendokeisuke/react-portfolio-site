dbbd3f59d69b5808ddc91b0cd757236a828439d3
遠藤圭輔 <endo.keisuke@pro.headwaters.co.jp>
2025-10-01 00:16:31 +0900

    画像の修正

--- src/components/Top.jsx
+++ src/components/Top.jsx
@@ -8,7 +8,7 @@ export const Top = () => {
             <div className="center">
               <h1 className="title-text">Welcome to my portfolio !</h1>
               <div className="arch-container">
-                <img src="/images/profile-image.png" alt="Profile" className="arch-image" />
+                <img src={process.env.PUBLIC_URL + "/images/profile-image.png"} alt="Profile" className="arch-image" />
               </div>
               <h2 className="name-text">遠藤　圭輔</h2>
               <a href="#about"><div className="next-icon"></div></a>
